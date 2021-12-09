33d48a33d3fe499a4346ceca41961e6e9d947d17
= <=>
2021-12-09 08:22:15 +0545

    Lab 1 Question 1

---
+++ lab-1/question-1/ConstructorDemo.java
@@ -0,0 +1,14 @@
+class ConstructorDemo{
+
+    // ConstructorDemo(){ //no arguments constructor
+    //     System.out.println("Inside Default Constructor");
+    // }
+
+    // ConstructorDemo(int a){ // parameterized constructor 
+    //     System.out.println("The value of a is " + a);
+    // }
+
+    int a = 7;
+    
+
+}
---
+++ lab-1/question-1/Main.java
@@ -0,0 +1,11 @@
+class Main{
+
+    public static void main(String[] args){
+
+        // ConstructorDemo obj = new ConstructorDemo(); // no arguments constructor
+        // ConstructorDemo obj = new ConstructorDemo(7); // parameterized
+        ConstructorDemo obj = new ConstructorDemo(); // default
+        System.out.println("Value of a is " + obj.a);
+
+    }
+}
