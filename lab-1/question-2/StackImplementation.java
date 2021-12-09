caeb8dbb9dde0bc45e4bc02555b9d2e2eb75f4c6
= <=>
2021-12-09 08:21:28 +0545

    Lab 1

---
+++ lab-1/question-2/StackImplementation.java
@@ -0,0 +1,28 @@
+class StackImplementation{
+
+    int top;
+    int arr[];
+
+    
+    StackImplementation(){
+        top = -1;
+        arr = new int[5];
+
+    }
+
+    boolean isFull(){
+        if(top == 5){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    boolean isFull(){
+        if(top == 5){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
